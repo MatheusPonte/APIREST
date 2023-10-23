4b850c61fd206cc9cce1bdc1a3884339c9ece8d6
MatheusPonte <MatheusPontes894@outlook.com>
2023-10-23 10:43:17 -0300

    Metodos com body

--- src/main/java/br/com/matheusponte/main/controller/userController.java
+++ src/main/java/br/com/matheusponte/main/controller/userController.java
@@ -1,9 +1,8 @@
 package br.com.matheusponte.main.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController // A classe controladora definindo, onde vai receber requisção// -> construindo uma app no conceito API REST
 @RequestMapping("/login") // Rota
@@ -12,5 +11,25 @@ public class userController {
     @GetMapping("/users/{id}")
     public String primeiroMetodo(@PathVariable String id){ // metodo junto com parametro
         return "o parametro é " + id;
+    }
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id){
+        return "O parametro com query é " + id;
+
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams){
+        return "O parametro com query é " + allParams.entrySet();
+
+    }
+
+    @PostMapping("/metodoComBody")
+    public String metodoComBodyParams(@RequestBody Usuario usuario){
+        return "metodoComBody " + usuario.username();
+    }
+
+    record Usuario(String username){
+
     }
 }
