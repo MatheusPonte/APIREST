34684c9eea3dd74f0b4948eaa547d3fb7e7de889
MatheusPonte <MatheusPontes894@outlook.com>
2023-10-24 09:06:47 -0300

    Fudamentos SpringBoot Completo

--- src/main/java/br/com/matheusponte/main/controller/userController.java
+++ src/main/java/br/com/matheusponte/main/controller/userController.java
@@ -1,8 +1,11 @@
 package br.com.matheusponte.main.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Objects;
 
 @RestController // A classe controladora definindo, onde vai receber requisção// -> construindo uma app no conceito API REST
 @RequestMapping("/login") // Rota
@@ -37,6 +40,15 @@ public class userController {
     @PostMapping("/metodoComListHeader")
     public String metodoComHeader(@RequestHeader Map<String, String> headers){
         return "metodoHeader " + headers.entrySet();
+    }
+
+    @GetMapping("/response/{id}")
+    public ResponseEntity<Object> metodoResponseEntity(@PathVariable Long id){
+        var usuario = new Usuario("Ponte");
+        if( id > 5){
+            return ResponseEntity.status(HttpStatus.OK).body(usuario);
+        }
+        return ResponseEntity.badRequest().body("menor que 5");
     }
 
     record Usuario(String username){
